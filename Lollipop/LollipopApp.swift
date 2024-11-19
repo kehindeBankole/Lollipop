8d152fcc4adcebbb29313be600be058b6a98f2ea
kehinde <bankolek1@gmail.com>
2024-11-19 15:13:30 +0100

    Initial Commit

---
+++ Lollipop/LollipopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LollipopApp.swift
+//  Lollipop
+//
+//  Created by Kehinde Bankole on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct LollipopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
